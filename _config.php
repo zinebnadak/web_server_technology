ded58c01b0dae8c3bc108d035e5d54ad42bd209b
MalinHager <malin.hager@ha.ax>
2026-08-31 09:05:43 +0300

    skrev in databaskontakten i _config.php filen

--- _config.php
+++ _config.php
@@ -2,4 +2,34 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+$dbhost = "localhost";
+$dbname = "thenest_umbrellacorp_top";
+$dbuser = "mysqluser";
+$dbpassword = "umbrella";
+
+// Create connection
+$conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+
+// Check connection
+if (!$conn)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Run SQL-statement
+if($result = mysqli_query($conn, "SELECT * FROM users WHERE employeecode='$employeecode' AND passwd='password'"))
+{
+    // Loopa genom resultat-arrayen
+    while($row = msqli_fetch_assoc($result))
+    {
+        // Läs in värden
+        $id = $row["id"];
+        $logintimes = $row["logintimes"];
+        $lastlogin = $row["lastlogin"];
+        $lockout = $row["lockout"];
+        $loggedin = "ok";
+    }
+}
+
 ?>
