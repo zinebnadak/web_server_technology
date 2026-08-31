7b6b4e354a28a9bc6d1e31fef08a8c1d6e80e04c
MalinHager <malin.hager@ha.ax>
2026-08-31 09:48:42 +0300

    Satte in en global variabel i _globals.php (siteaddress),lade även till siteadress echo i _master_info_middle.php,

--- _globals.php
+++ _globals.php
@@ -1,4 +1,7 @@
 <?php
+// Globala variabler
+$siteaddress = "thenest.umbrellacorp.top";
+
 
 //Läs in globala funktioner
 include ("_f_readtextfile.php");
--- _security.php
+++ _security.php
@@ -2,15 +2,15 @@
 // Möjliggör sessions
 session_start();
 
-// Skapa sessions-variabler
-$_SESSION["loggedin"] = "ok";
+// Skapa sessions-variabler ( tex vid inloggning)
+/*$_SESSION["loggedin"] = "ok";
 $_SESSION["employeecode"] = $employeecode;
 
-// Dödar sessions
+// Dödar sessions (tex vid utloggning)
 unset($_SESSION["loggedin"]);
 unset($_SESSION["employeecode"]);
 
 // Dödar alla sessions-variabler
-session_destroy();
+session_destroy();*/
 
 ?>
