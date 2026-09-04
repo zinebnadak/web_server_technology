a638324e9b670fa47d2c64211477acbe0e639fe5
MalinHager <malin.hager@gmail.com>
2026-09-04 07:06:46 +0300

    test: connection to database test, succeded

--- _config.php
+++ _config.php
@@ -2,22 +2,18 @@
 //echo phpinfo();
 
 // ----------------- DISPLAY ALL ERRORS ------------------------
-ini_set ('display_errors', 1);
-ini_set ('display_startup_errors', 1);
-error_reporting (E_ALL);
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // ----------------- DATABASE CONNECTION -----------------------
 $dbhost = "localhost";
-$dbname = "www_umbrellacorp_top"; 
+$dbname = "thenest_umbrellacorp_top";
 $dbuser = "mysqluser";
-$dbpassword = "umbrella";
-$conn = new mysqli($dbhost, $dbuser, $dbpassword); 
-if($conn->connect_error)
-{
-    die("Connection failed: ".$conn->connect_error);
+$dbpassword = "thenest_appuser"; 
+
+$conn = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-
-
 ?>
